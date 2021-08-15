a5b1a1a26167484d9a477610628b59962b2a4b18
Universe-c0de <85509930+Universe-c0de@users.noreply.github.com>
2021-08-15 16:11:54 +0300

    migrations

--- django_project/main/admin.py
+++ django_project/main/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from .models import Task
+from .models import Task, Venue, ToDoUser, Event
+
+
+admin.site.register(Task)
+admin.site.register(Venue)
+admin.site.register(ToDoUser)
+admin.site.register(Event)
 
 
 # patterns = [
 #     path('members/', include('django.contrib.auth.urls')),
 #     path('members/', include('members.urls'))
 # ]
-
-
-admin.site.register(Task)
--- django_project/main/models.py
+++ django_project/main/models.py
@@ -1,6 +1,41 @@
 from django.db import models
 
 
+class Venue(models.Model):
+    name = models.CharField('Venue Name', max_length=120)
+    address = models.CharField(max_length=300)
+    phone = models.CharField('Contact Phone', max_length=25)
+    web = models.URLField('Website Address')
+    email_address = models.EmailField('Email Address')
+
+
+    def __str__(self):
+        return self.name
+
+
+class ToDoUser(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    Email = models.EmailField('User Email')
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Event(models.Model):
+    name = models.CharField('Event Name', max_length=120)
+    event_date = models.DateTimeField('Event_Date')
+    venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
+    # venue = models.CharField(max_length=120)
+    manager = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+    attendees = models.ManyToManyField(ToDoUser, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+
 class Task(models.Model):
     title = models.CharField('название', max_length=50)
     task = models.TextField('Описание')
